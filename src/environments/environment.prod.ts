f7ecfb534955f63183584b058b03bc4d2fb788d3
mardbrito <marcelo4web@gmail.com>
2021-10-26 21:27:18 -0400

    Fix apiKey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiKey: 'bc6c0092aeb617d75f81a4070f5a7ec7'
+  apiKey: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiKey: 'bc6c0092aeb617d75f81a4070f5a7ec7'
+  apiKey: ''
 };
 
 /*
